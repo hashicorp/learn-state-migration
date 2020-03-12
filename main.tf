e859936ec6725486aec8e9707219915e7e696158
justinretzolk <jretzolk@hashicorp.com>
2020-03-12 14:47:22 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Terraform configuration
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "3"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
